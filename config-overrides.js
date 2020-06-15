aa5b139a5befb2c64188a40a4b9752e75c5100c5
inier <inier@qq.com>
2020-06-16 00:01:39 +0800

    fix: 更新

--- config-overrides.js
+++ config-overrides.js
@@ -77,6 +77,6 @@ module.exports = override(
     }),
     // 开启打包速度分析
     // speedMeasure(),
-    BUNDLE_VISUALIZE === 'true' && addBundleVisualizer()
+    (process.env.NODE_ENV === 'production' || BUNDLE_VISUALIZE === 'true') && addBundleVisualizer()
     // prerender(),
 );
